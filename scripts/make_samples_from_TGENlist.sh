c82334c25353c3aa20511e8f053e41edb0a230d0
Jason Stajich <jasonstajich.phd@gmail.com>
2022-12-04 09:27:22 -0800

    script adding tgen new samples to samples.csv

---
+++ scripts/make_samples_from_TGENlist.sh
@@ -0,0 +1,5 @@
+cd input
+
+ls *_R1_001.fastq.gz | perl -p -e 's/(\S+)(_L\d+)_R1_001.fastq.gz/$1$2,$1,TGEN-NORTH,,Coccidioides immitis/; s/,Coccidioides_spp_/,/; s/_[ACGT]+\-[ACGT]+,/,/; s/,COCSP\-/,/; s/,(\S+)\-uu\S+BB_S\d+,/,$1,/; s/,NACVFR_Cocci_(\S+)_S\d+,/,$1,/' > samples.csv
+cat samples.csv >> ../samples.csv
+rm samples
